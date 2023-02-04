1925d15242d44dafad52009dfac1057ad1c6479f
Voncloft <voncloft@gmail.com>
2023-02-04 18:44:00 -0500

    Minor Updates

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-
-?>
+<center><b><u>Exercises</u></b>
+<table border=2><tr><th>Aerobic</th><th>Muscle</th></tr>
+<tr><td valign='top'>
+	<table>
+		<tr><td><a target=_blank href='php/Treadmill.php'>Treadmill</a></td></tr>
+	</table>
+</td><td valign='top'>
+	<table>
+		<tr><td><a target=_blank href='php/Curling.php'>Curling</a></td></tr>
+		<tr><td><a target=_blank href='php/Single_weights.php'>Single Weights</a></td></tr>
+		<tr><td><a target=_blank href='php/Planks.php'>Planks</a></td></tr>
+		<tr><td><a target=_blank href='php/Twist_Reps.php'>Twist Reps</a></td></tr>
+		<tr><td><a target=_blank href='php/Pushups.php'>Pushups</a></td></tr>
+		<tr><td><a target=_blank href='php/Tricep_curls.php'>Tricep Curls</a></td></tr>
+		<tr><td><a target=_blank href='php/Butterflies.php'>Butterflies</a></td></tr>
+	</table>
+</td></tr></table>
+<a target=_blank href='php/show_history.php'>Show History</a><br>
+TODO: Fix muscle exercises where no weights are needed
